eca1ba234d6db840e1ca45ce883fde2952b89fc5
yujun-yeong <yujun-yeong@naver.com>
2024-06-20 12:56:09 +0900

    준형님이랑 겹친부분 확인용

---
+++ RM/src/main/java/com/kh/reMerge/feed/model/dao/StoryDao.java
@@ -0,0 +1,14 @@
+package com.kh.reMerge.feed.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.kh.reMerge.feed.model.vo.Story;
+
+public class StoryDao {
+
+	public int insertStory(SqlSessionTemplate sqlSession, Story s) {
+
+		return 0;
+	}
+
+}
