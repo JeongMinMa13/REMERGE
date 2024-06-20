d522d57528f0fec53c13e8a27e27fc1d6c82477c
junhyunging <akddnjs0807@naver.com>
2024-06-20 09:55:56 +0900

    이동 적음

--- RM/src/main/java/com/kh/reMerge/feed/controller/FeedController.java
+++ RM/src/main/java/com/kh/reMerge/feed/controller/FeedController.java
@@ -1,9 +1,17 @@
 package com.kh.reMerge.feed.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class FeedController {
+	
+	@GetMapping("feed.fe")
+	public String feed() {
+		
+		return "feed/mainFeed";
+	}
 
+	
 }
